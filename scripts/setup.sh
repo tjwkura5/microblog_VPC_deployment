e170a9b8756043913ed317e393ac50b0626818f8
Terry Woolard <terrywoolard@Terrys-MBP.attlocal.net>
2024-09-22 13:21:31 -0400

    add more comments

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Variables
-LOCAL_FILE_PATH="/home/ubuntu/scripts/start_app.sh"  
+FILE_PATH="/home/ubuntu/scripts/start_app.sh"  
 REMOTE_IP="10.0.132.86"                
-REMOTE_FILE_PATH="/home/ubuntu/scripts/start_app.sh" 
 SSH_KEY="/home/ubuntu/.ssh/app-key.pem"
 
 # Step 1: Secure copy the file to the remote server
-scp -i "${SSH_KEY}" -o StrictHostKeyChecking=no "${LOCAL_FILE_PATH}" ubuntu@"${REMOTE_IP}":"${REMOTE_FILE_PATH}"
+scp -i "${SSH_KEY}" -o StrictHostKeyChecking=no "${FILE_PATH}" ubuntu@"${REMOTE_IP}":"${FILE_PATH}"
 
+# Check the exit status of the scp command
+# $? holds the exit status of the last executed command (0 means success, non-zero means failure)
 if [ $? -eq 0 ]; then
     echo "File copied successfully."
 else
@@ -16,7 +17,9 @@ else
     exit 1
 fi
 
-# Step 2: SSH into the remote server and print its IP address
+# Step 2: SSH into the remote server
+# The '<< 'EOF'' syntax is used to create a "here document," which allows you to send multiple lines of input to a command
+# Everything between 'EOF' and 'EOF' is treated as input to the SSH command
 ssh -i "${SSH_KEY}" -o StrictHostKeyChecking=no ubuntu@"${REMOTE_IP}" << 'EOF'
     if [ $? -ne 0 ]; then
         echo "Failed to connect to the remote server."
